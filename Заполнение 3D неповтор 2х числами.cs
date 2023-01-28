262749fbcf435147b6cc7ed0eefd58b5d32b4f8b
Qodrew <120235371+Qodrew@users.noreply.github.com>
2023-01-28 18:37:48 +0300

    Create Заполнение 3D неповтор 2х числами.cs

---
+++ Заполнение 3D неповтор 2х числами.cs
@@ -0,0 +1,36 @@
+// заполнение трехмерного массива неповторяющимися двузначными числами
+void FillArray(int[,,] inputArray)
+{
+  int[] temp = new int[inputArray.GetLength(0) * inputArray.GetLength(1) * inputArray.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < inputArray.GetLength(0); x++)
+  {
+    for (int y = 0; y < inputArray.GetLength(1); y++)
+    {
+      for (int z = 0; z < inputArray.GetLength(2); z++)
+      {
+        inputArray[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
